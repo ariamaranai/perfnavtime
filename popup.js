c5e5d2da0c0f6ca0379dbda2d8649b024844fb0b
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-07 23:44:33 +0900

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,27 @@
+chrome.tabs.query({ active: !0, currentWindow: !0 }).then(tabs =>
+  tabs[0].url[0] != "c" && chrome.scripting.executeScript({
+    target: { tabId: tabs[0].id },
+    world: "MAIN",
+    func: () => {
+      let entry = performance.getEntriesByType("navigation")[0];
+      let duration = entry.duration.toFixed(1);
+      let f = t => t.toFixed(1).padStart(duration.length, " ") + "ms";
+      return [
+        entry.nextHopProtocol,
+        entry.deliveryType,
+        entry.encodedBodySize,
+        entry.decodedBodySize,
+       (entry.encodedBodySize / entry.decodedBodySize * 100).toFixed(1) + "%",
+        duration + "ms",
+       f(entry.requestStart - entry.secureConnectionStart),
+       f(entry.responseEnd - entry.responseStart),
+       f(entry.domContentLoadedEventEnd - entry.domContentLoadedEventStart),
+       f(entry.domComplete - entry.domInteractive)
+      ]
+    }
+  }, results => {
+    let u = document.getElementsByTagName("u");
+    let i = 10;
+    while (u[--i].textContent = results[0].result[i], i);
+  })
+);
