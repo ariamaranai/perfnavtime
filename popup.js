7767bf709087a03b9e1a22626c4312b90d358591
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-08 22:31:17 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -6,12 +6,13 @@ chrome.tabs.query({ active: !0, currentWindow: !0 }).then(tabs =>
       let entry = performance.getEntriesByType("navigation")[0];
       let duration = entry.duration.toFixed(1);
       let f = t => t.toFixed(1).padStart(duration.length, " ") + "ms";
+      let ratio = (entry.encodedBodySize / entry.decodedBodySize * 100).toFixed(1) + "%";
       return [
         entry.nextHopProtocol,
         entry.deliveryType,
-        entry.encodedBodySize,
-        entry.decodedBodySize,
-       (entry.encodedBodySize / entry.decodedBodySize * 100).toFixed(1) + "%",
+        entry.encodedBodySize.toString().padStart(ratio.length, " "),
+        entry.decodedBodySize.toString().padStart(ratio.length, " "),
+        ratio,
         duration + "ms",
        f(entry.requestStart - entry.secureConnectionStart),
        f(entry.responseEnd - entry.responseStart),
