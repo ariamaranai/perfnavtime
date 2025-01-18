1ad784bbefb4ee6dee2097201a5974d8c5dbfaa6
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-18 23:06:54 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,40 +1,50 @@
 chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
-  let url = tabs[0].url;
+  let t = tabs[0];
+  let url = t.url;
   url[0] != "c" && chrome.scripting.executeScript({
-    target: { tabId: tabs[0].id },
+    target: { tabId: t.id },
     world: "MAIN",
-    func: async () => {
-      let entry = performance.getEntriesByType("navigation")[0];
-      let { encodedBodySize, decodedBodySize } = entry;
+    func: () => {
+      let {
+        nextHopProtocol,
+        deliveryType,
+        encodedBodySize,
+        decodedBodySize,
+        duration,
+        requestStart,
+        secureConnectionStart,
+        responseEnd,
+        responseStart,
+        domContentLoadedEventEnd,
+        domContentLoadedEventStart,
+        domComplete,
+        domInteractive
+      } = performance.getEntriesByType("navigation")[0];
       return [
-        entry.nextHopProtocol,
-        entry.deliveryType,
+        nextHopProtocol,
+        deliveryType,
         encodedBodySize,
         decodedBodySize.toString(),
         encodedBodySize == decodedBodySize ? "100%" : (encodedBodySize / decodedBodySize * 100).toFixed(1) + "%",
-        "",
-        "",
-        entry.duration.toFixed(1) + "ms",
-        (entry.requestStart - entry.secureConnectionStart).toFixed(1) + "ms",
-        (entry.responseEnd - entry.responseStart).toFixed(1) + "ms",
-        (entry.domContentLoadedEventEnd - entry.domContentLoadedEventStart).toFixed(1) + "ms",
-        (entry.domComplete - entry.domInteractive).toFixed(1) + "ms"
+        0,
+        0,
+        duration.toFixed(1) + "ms",
+        (requestStart - secureConnectionStart).toFixed(1) + "ms",
+        (responseEnd - responseStart).toFixed(1) + "ms",
+        (domContentLoadedEventEnd - domContentLoadedEventStart).toFixed(1) + "ms",
+        (domComplete - domInteractive).toFixed(1) + "ms"
       ];
     }
   }, async results => {
     if (results &&= results[0].result) {
       if (results[4] == "100%") {
-        let res = await fetch (url);
-        if (res.status == 200) {
+        let response = await fetch (url);
+        if (response.status == 200) {
           results[5] = 0;
-          let stream = await (await res.body).pipeThrough(new CompressionStream("gzip"));
-          let reader = stream.getReader();
+          let reader = (await (await response.body).pipeThrough(new CompressionStream("gzip"))).getReader();
           let chunk;
-
-          while (!(chunk = await reader.read()).done) {
-            console.log(chunk.value);
+          while (!(chunk = await reader.read()).done)
             results[5] += chunk.value.length;
-          }
           results[6] = (results[5] / results[2] * 100).toFixed(1) + "%";
           results[5] = results[5].toString();
         }
@@ -43,7 +53,10 @@ chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
       let maxLength = Math.max(results[1].length, results[3].length, results[4].length, results[7].length);
       let u = document.body.children;
       let i = 12;
-      while (u[--i].textContent = results[i].padStart(maxLength, " "), i);
+      while (
+        u[--i].textContent = (t = results[i]) ? t.padStart(maxLength, " ") : "", 
+        i
+      );
     }
-  })
+  });
 });
