f64195f4d35a03e62ea4bfe50a77f7b6067bd3f6
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-13 20:39:01 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -23,7 +23,7 @@ chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
         nextHopProtocol,
         deliveryType,
         encodedBodySize,
-        decodedBodySize.toString(),
+        decodedBodySize + "",
         encodedBodySize == decodedBodySize ? "100%" : (encodedBodySize / decodedBodySize * 100).toFixed(1) + "%",
         0,
         0,
@@ -36,16 +36,20 @@ chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
     }
   }, async results => {
     if (results &&= results[0].result) {
-      if (results[4] == "100%") {
+      let encodedBodySize = results[2];
+      let compressionRatio = results[4];
+      if (compressionRatio == "100%") {
         let response = await fetch (url);
         if (response.status == 200) {
-          results[5] = (await (new Response(response.body.pipeThrough(new CompressionStream("gzip")))).bytes()).length;
-          results[6] = (results[5] / results[2] * 100).toFixed(1) + "%";
-          results[5] = results[5].toString();
+          let bytes = results[5] = (await (
+            new Response(response.body.pipeThrough(new CompressionStream("gzip")))
+          ).bytes()).length;
+          results[6] = (bytes / encodedBodySize * 100).toFixed(1) + "%";
+          results[5] = bytes + "";
         }
       }
-      results[2] = results[2].toString();
-      let maxLength = Math.max(results[1].length, results[3].length, results[4].length, results[7].length);
+      results[2] = encodedBodySize + "";
+      let maxLength = Math.max(results[1].length, results[3].length, compressionRatio.length, results[7].length);
       let u = document.body.children;
       let i = 12;
       while (
